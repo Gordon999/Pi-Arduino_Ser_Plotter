efe6389bb1d651b28b819a11f501a8b834a812fc
RIN67630 <lazlo.lebrun@googlemail.com>
2020-03-06 20:49:00 +0100

    Just streamlined, put parameters together

--- Pi-Arduino_Ser_Plotter.py
+++ Pi-Arduino_Ser_Plotter.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python3
+
+# getting libraries
 import serial
 import os, sys
 import threading
@@ -9,18 +11,36 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
-from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
-                               AutoMinorLocator)
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator)
+
+# configuration
+serial_port = '/dev/ttyUSB0'
+plot_length = 360
+log_length = 86400
+log = 1 # 1 = log in RAM | 2 = odd/even logs in RAM every hour
+
+# beautifying plot params
+plt.style.use('seaborn-darkgrid')
+plt.rc('figure', facecolor='darkgrey')
+plt.rc('figure.subplot', left=0.06, right=0.99, top=0.99, bottom=0.08)
+plt.rc('xtick', labelsize=6)     
+plt.rc('ytick', labelsize=6)
+plt.rc('lines', linewidth=1)
+plt.rc('legend', frameon=True, loc='upper left', facecolor='white', framealpha=0.5, fontsize=7)
+plt.rc('font', size=9)
+plt.ioff()
+#kill the useless Matplotlib toolbar
+plt.rcParams['toolbar'] = 'None'
 
 if os.path.exists('/run/shm/example.txt'): # note log currently saved to RAM if log = 1
    os.remove('/run/shm/example.txt')       # gets deleted as you restart the script
 
-ser = serial.Serial(port='/dev/ttyUSB0',baudrate = 9600,parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
+ser = serial.Serial(port= serial_port)
+                    #omitted: ,baudrate = 9600,parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
 
-# set variables
-global xs,y1,y2,y3,y4,y5,max_count,max_log, count,list_lock,start
+                    # set variables
+global xs,y1,y2,y3,y4,y5,max_count,count,list_lock,start
 max_count = 360
-max_log = 86400
 count = 0
 log = 1 # set log to 1 to save a log in RAM.
 xs = []
@@ -31,36 +51,22 @@ y4 = []
 y5 = []
 start = 1
 list_lock = 0
+#print("Day of the year:" + time.strftime("%j"))
 
 def onclick(x):
    global start
    start = 0
-
-#kill the toolbar
-plt.rcParams['toolbar'] = 'None'
-
-# beautify
-
-plt.style.use('seaborn-darkgrid')
-plt.rc('figure', facecolor='darkgrey')
-plt.rc('figure.subplot', left=0.06, right=0.99, top=0.99, bottom=0.08)
-plt.rc('xtick', labelsize=6)     
-plt.rc('ytick', labelsize=6)
-plt.rc('lines', linewidth=1)
-plt.rc('legend', frameon=True, loc='upper left', facecolor='white', framealpha=0.5, fontsize=7)
-plt.rc('font', size=9)
-plt.ioff()
       
 def thread_plot():
     global fig,animate, ax1
-    fig = plt.figure("Sound Pressure Level")
+    fig = plt.figure("Sound Pressure Level from" + serial_port)
     ax1 = fig.add_subplot(1,1,1)
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
-    ani = animation.FuncAnimation(fig, animate, interval=10000)
+    ani = animation.FuncAnimation(fig, animate, interval=10000) #update every 10sec
     plt.show()
    
 def animate(i):
-    global xs,y1,y2,y3,y4,y5,max_count,count,list_lock,start
+    global xs,y1,y2,y3,y4,y5,plot_length,count,list_lock,start
     if count > 0 and start == 1:
         ax1.clear()
         plt.xlabel('click on the plot to stop on next update') 
@@ -70,11 +76,11 @@ def animate(i):
         while list_lock == 1:
             time.sleep(0.01)
         list_lock = 1
-        ax1.plot(xs, y1, '-.b', label='Avg')
-        ax1.plot(xs, y2, '-r',  label='A0')
-        ax1.plot(xs, y3, '-g',  label='A0Slow')
-        ax1.plot(xs, y4, ':c',  label='Min')
-        ax1.plot(xs, y5, ':y',  label='Max')
+        ax1.plot(xs, y1, '-.b', label='Avg')     # dash-dotted line, blue
+        ax1.plot(xs, y2, '-r',  label='A0')      # solid line, red
+        ax1.plot(xs, y3, '-g',  label='A0Slow')  # solid line, green
+        ax1.plot(xs, y4, ':c',  label='Min')     # dotted line, cyan
+        ax1.plot(xs, y5, ':y',  label='Max')     # dotted line, yellow
         ax1.legend(loc='upper left')
         list_lock = 0
     if count > 0 and start == 0:
@@ -94,7 +100,7 @@ if __name__ == '__main__':
     now = datetime.datetime.now()
     t = str(now)[11:19]                     # current time as hhmmss
                                             # check for linetype
-    linetype = "#"                          # preset with error
+    linetype = "#"                          # preset with error, should be overwitten next
     if counter1 == 0 and counter2 == 0 and counter3 == 0:     # waiting for sync
       linetype = "s"
     if counter3 == 4  :                     # header line
@@ -114,7 +120,7 @@ if __name__ == '__main__':
       y4.append(float(e))
       y5.append(float(f))
       # delete old list values
-      if len(xs) > max_count:
+      if len(xs) > plot_length:
             del xs[0]
             del y1[0]
             del y2[0]
@@ -123,15 +129,13 @@ if __name__ == '__main__':
             del y5[0]
       list_lock = 0
       count +=1
-
     if log == 1:                          # save to log file  
       timestamp = now.strftime("%y/%m/%d-%H:%M:%S")
       with open('/run/shm/example.txt', 'a') as g:
-         g.write(timestamp + " " + str(count)+" " + Ard_data +" "+ linetype)
+         g.write(timestamp + " " + str(count)+ " " + Ard_data + " " + linetype + "\n")
 
     if linetype == "#":
        print("unexpected serial data, got that:[ " + Ard_data + "] please check")
   
   ser.close()
   sys.exit()
-  
