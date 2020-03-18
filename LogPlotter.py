27b274835e5e865db70589a18b9f018780a7f761
RIN67630 <lazlo.lebrun@googlemail.com>
2020-03-18 16:43:03 +0100

    Update LogPlotter.py
    No functional program change: Just consolidated the configuration at the beginning

--- LogPlotter.py
+++ LogPlotter.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
+# Remark: this script needs the module tkcalendar
+# install it with pip3 install tkcalendar before running
 
-# LogPLotter27
 
 # getting libraries
 import os
@@ -16,41 +17,78 @@ import matplotlib.animation as animation
 from matplotlib.ticker import (MultipleLocator)
 from tkcalendar import DateEntry
 
+# ***CONFIGURATION***
+log_path = '/home/pi/Noise/'
+plot_tracks = 5    #not yet relevant...
+plot_length = 360
+page_length = 300  #not yet relevant...
+plot_labels = ('Avg','A0','A0Slow','Min','Max')
+plot_styles = ('-.b','-r','-g',':c',':y')
+    #plot styles summary, any combination of line syle an color is allowed:
+        # -  solid line
+        # :  dotted line
+        # -. dash-dotted line
+        # r g b m c y k   for red, green blue, magenta, cyan, black
+
+# beautifying plot params
+plt.style.use('seaborn-darkgrid')
+plt.rc('figure', facecolor='darkgrey')
+plt.rc('figure.subplot', left=0.06, right=0.99, top=0.99, bottom=0.08)
+plt.rc('xtick', labelsize=6)     
+plt.rc('ytick', labelsize=6)
+plt.rc('lines', linewidth=1)
+plt.rc('legend', frameon=True, loc='upper left', facecolor='white', framealpha=0.5, fontsize=7)
+plt.rc('font', size=9)
+plt.ioff()
+
+#kill the useless Matplotlib toolbar
+plt.rcParams['toolbar'] = 'None'
+# *** END of CONFIGURATION ***
+
+
 class Plot(Frame):
+    def animate(self, i):
+        if self.count > 0 and self.start == 1:
+            self.ax1.clear()
+            if self.legend == 1:
+                plt.xlabel('Mid Time: ' + self.mid_time3 + ' Log: ' + self.log_file[len(log_path):] + " - LEFT for PREVIOUS, RIGHT for NEXT")
+            elif self.legend == 0:
+                plt.xlabel('Mid Time: ' + self.mid_time3 + ' Log: ' + self.log_file[len(log_path):] + " - RIGHT for NEXT")
+            elif self.legend == 2:
+                plt.xlabel('Mid Time: ' + self.mid_time3 + ' Log: ' + self.log_file[len(log_path):] + " - LEFT for PREVIOUS") 
+            plt.ylabel('dB')
+            self.ax1.xaxis.set_major_locator(MultipleLocator(30))
+            self.ax1.xaxis.set_minor_locator(MultipleLocator(10))
+            while self.list_lock == 1:
+                time.sleep(0.01)
+            self.list_lock = 1
+            self.ax1.plot(self.xs, self.y1, plot_styles[0], label = plot_labels[0] ) 
+            self.ax1.plot(self.xs, self.y2, plot_styles[1], label = plot_labels[1] )
+            self.ax1.plot(self.xs, self.y3, plot_styles[2], label = plot_labels[2] )
+            self.ax1.plot(self.xs, self.y4, plot_styles[3], label = plot_labels[3] )
+            self.ax1.plot(self.xs, self.y5, plot_styles[4], label = plot_labels[4] )
+            self.ax1.legend(loc='upper left')
+            self.list_lock = 0
+        if self.count > 0 and self.start == 0 :
+            plt.close('all')
+
     def __init__(self):
         super().__init__() 
         self.initUI()
 
     def initUI(self):
-        # configuration
-        self.plot_length = 360
-        self.log_path = '/home/pi/Noise/'
-        
         # set variables
         self.count = 0
-        self.xs = []
-        self.y1 = []
-        self.y2 = []
-        self.y3 = []
-        self.y4 = []
-        self.y5 = []
+        self.xs = [str]
+        self.y1 = [str]
+        self.y2 = [str]
+        self.y3 = [str]
+        self.y4 = [str]
+        self.y5 = [str]
         self.start = 1
         self.list_lock = 0
         self.s_plot = 0
         self.legend = 1
-
-        # beautifying plot params
-        plt.style.use('seaborn-darkgrid')
-        plt.rc('figure', facecolor='darkgrey')
-        plt.rc('figure.subplot', left=0.06, right=0.99, top=0.99, bottom=0.08)
-        plt.rc('xtick', labelsize=6)     
-        plt.rc('ytick', labelsize=6)
-        plt.rc('lines', linewidth=1)
-        plt.rc('legend', frameon=True, loc='upper left', facecolor='white', framealpha=0.5, fontsize=7)
-        plt.rc('font', size=9)
-        plt.ioff()
-        #kill the useless Matplotlib toolbar
-        plt.rcParams['toolbar'] = 'None'
 
         # setup tkinter window
         Frame10 = tk.Frame(width=300, height=60)
@@ -103,18 +141,19 @@ class Plot(Frame):
     def click(self,event):
         # RIGHT click (NEXT)
         if event.button == 3:
-            self.mid_count += self.plot_length - 1
+            self.mid_count += plot_length  - 1
+            self.mid_time3 = str(datetime.datetime.strptime(self.mid_time3,'%H:%M:%S') + TimeDelta(seconds=plot_length ))[11:19]
             self.legend = 1
-            if self.mid_count + int(self.plot_length/2) > self.count_x:
-                self.mid_count = self.count_x - int(self.plot_length/2)
+            if self.mid_count + int(plot_length /2) > self.count_x:
+                self.mid_count = self.count_x - int(plot_length /2)
                 self.legend = 2
             self.read_plot()
         # LEFT click (PREVIOUS)
         elif event.button == 1 :
-            self.mid_count -= self.plot_length -1
+            self.mid_count -= plot_length  -1
             self.legend = 1
-            if self.mid_count < int(self.plot_length/2) + 1:
-                self.mid_count = int(self.plot_length/2) + 1
+            if self.mid_count < int(plot_length /2) + 1:
+                self.mid_count = int(plot_length /2) + 1
                 self.legend = 0
             self.read_plot()
 
@@ -130,7 +169,7 @@ class Plot(Frame):
         self.start = 0
         # set mid_count to 0, forcing log file to be read.
         self.mid_count = 0
-        self.log_file = self.log_path + dat + ".log"
+        self.log_file = log_path + dat + ".log"
         self.read_plot()
     
     def thread_plot(self):
@@ -140,31 +179,6 @@ class Plot(Frame):
         self.ani = animation.FuncAnimation(self.fig, self.animate, interval=100) 
         plt.show()
    
-    def animate(self, i):
-        if self.count > 0 and self.start == 1:
-            self.ax1.clear()
-            if self.legend == 1:
-                plt.xlabel('Mid Time: ' + self.mid_time3 + ' Log: ' + self.log_file[len(self.log_path):] + " - LEFT for PREVIOUS, RIGHT for NEXT")
-            elif self.legend == 0:
-                plt.xlabel('Mid Time: ' + self.mid_time3 + ' Log: ' + self.log_file[len(self.log_path):] + " - RIGHT for NEXT")
-            elif self.legend == 2:
-                plt.xlabel('Mid Time: ' + self.mid_time3 + ' Log: ' + self.log_file[len(self.log_path):] + " - LEFT for PREVIOUS") 
-            plt.ylabel('dB')
-            self.ax1.xaxis.set_major_locator(MultipleLocator(30))
-            self.ax1.xaxis.set_minor_locator(MultipleLocator(10))
-            while self.list_lock == 1:
-                time.sleep(0.01)
-            self.list_lock = 1
-            self.ax1.plot(self.xs, self.y1, '-.b', label='Avg')     # dash-dotted line, blue
-            self.ax1.plot(self.xs, self.y2, '-r',  label='A0')      # solid line, red
-            self.ax1.plot(self.xs, self.y3, '-g',  label='A0Slow')  # solid line, green
-            self.ax1.plot(self.xs, self.y4, ':c',  label='Min')     # dotted line, cyan
-            self.ax1.plot(self.xs, self.y5, ':y',  label='Max')     # dotted line, yellow
-            self.ax1.legend(loc='upper left')
-            self.list_lock = 0
-        if self.count > 0 and self.start == 0 :
-            plt.close('all')
-
     def read_plot(self):
         if os.path.exists (self.log_file):
             self.xs = []
@@ -217,17 +231,15 @@ class Plot(Frame):
                         objFile.close()
                         
                 # calculate start_count and end_count from mid_count and plot_length
-                start_count = self.mid_count - int(self.plot_length/2)
-                end_count = self.mid_count + int(self.plot_length/2)
-                
+                start_count = self.mid_count - int(plot_length /2)
+                end_count = self.mid_count + int(plot_length /2)
                 # ensure not exceeding the ends of the log
                 if start_count < 1:
                     start_count = 1
-                    end_count = start_count + self.plot_length
+                    end_count = start_count + plot_length 
                 elif end_count > self.count_x:
                     end_count = self.count_x
-                    start_count = end_count - self.plot_length
-                    
+                    start_count = end_count - plot_length 
                 # call subrocess for temp log
                 path = "sed -n " +  str(start_count)  + "," +  str(end_count) + "p " + '"' + self.log_file + '"' + " >> /run/shm/plot.log"
                 p = subprocess.Popen(path, shell=True, preexec_fn=os.setsid)
@@ -251,15 +263,15 @@ class Plot(Frame):
                         line = file.readline()
                         self.log.append(line)
             # put values in lists for plotting
-            if len(self.log) >= self.plot_length:
-                for x in range(0,self.plot_length):
+            if len(self.log) >= plot_length :
+                for x in range(0,plot_length ):
                     if self.log[x][0:1] == "d":
                         counter1 = self.log[x].count(' ')
                         counter2 = self.log[x].count('.')
                         if counter1 == 6 and counter2 == 5:
                             a,t,b,c,d,e,f = self.log[x].split(" ")
                             # find mid_time to display
-                            if x == int(self.plot_length/2) + 1:
+                            if x == int(plot_length/2) + 1:
                                 self.mid_time3 = t
                             self.xs.append(t)
                             self.y1.append(float(b))
@@ -279,4 +291,4 @@ def main():
     root.mainloop() 
 
 if __name__ == '__main__':
-    main()  
+    main()
