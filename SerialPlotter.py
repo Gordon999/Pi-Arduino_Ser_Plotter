fdbb3fb10fa9bb14ae05b36ac2971644578d46ec
RIN67630 <lazlo.lebrun@googlemail.com>
2020-03-24 06:06:40 +0100

    A last roundup...
    Configuration variables for log options and conversion of 0,1 to boolean, stripping trailing spaces.

--- SerialPlotter.py
+++ SerialPlotter.py
@@ -15,11 +15,13 @@ from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLoc
 
 # configuration
 serial_port = '/dev/ttyUSB1'
-log = 1
+log = True
 log_path = '/home/pi/Noise/'
-plot_tracks = 5    #not yet relevant...
+plot_tracks = 5             #not yet relevant...
 plot_length = 360
-page_length = 300  #only relevant for PolLog.py
+page_length = 300           #only relevant for LogPlotter.py
+plot_interval = 10
+plot_title = 'Sound pressure level'
 plot_labels = ('Avg','A0','A0Slow','Min','Max')
 plot_styles = ('-.b','-r','-g',':c',':y')
     #plot styles summary, any combination of line syle an color is allowed:
@@ -32,7 +34,7 @@ plot_styles = ('-.b','-r','-g',':c',':y')
 plt.style.use('seaborn-darkgrid')
 plt.rc('figure', facecolor='darkgrey')
 plt.rc('figure.subplot', left=0.06, right=0.99, top=0.99, bottom=0.08)
-plt.rc('xtick', labelsize=6)     
+plt.rc('xtick', labelsize=6)
 plt.rc('ytick', labelsize=6)
 plt.rc('lines', linewidth=1)
 plt.rc('legend', frameon=True, loc='upper left', facecolor='white', framealpha=0.5, fontsize=7)
@@ -45,36 +47,35 @@ plt.rcParams['toolbar'] = 'None'
 
 def animate(i):
     global xs,y1,y2,y3,y4,y5,plot_length,count,list_lock,start
-    if count > 0 and start == 1:
+    if count > 0 and start:
         ax1.clear()
-        plt.xlabel('click on the plot to stop on next update') 
+        plt.xlabel('click on the plot to stop on next update')
         plt.ylabel('dB')
         ax1.xaxis.set_major_locator(MultipleLocator(30))
         ax1.xaxis.set_minor_locator(MultipleLocator(10))
         while list_lock == 1:
              time.sleep(0.01)
-        list_lock = 1
-        ax1.plot(xs, y1, plot_styles[0], label = plot_labels[0] ) 
+        list_lock = True
+        ax1.plot(xs, y1, plot_styles[0], label = plot_labels[0] )
         ax1.plot(xs, y2, plot_styles[1], label = plot_labels[1] )
         ax1.plot(xs, y3, plot_styles[2], label = plot_labels[2] )
         ax1.plot(xs, y4, plot_styles[3], label = plot_labels[3] )
         ax1.plot(xs, y5, plot_styles[4], label = plot_labels[4] )
         ax1.legend(loc='upper left')
-        list_lock = 0
-    if count > 0 and start == 0:
-       plt.close('all')
-       
-      
+        list_lock = False
+    if count > 0 and not start:
+        plt.close('all')
+
 def thread_plot():
     global fig,animate, ax1
-    fig = plt.figure("Sound Pressure Level from" + serial_port)
+    fig = plt.figure(plot_title + serial_port)
     ax1 = fig.add_subplot(1,1,1)
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
-    ani = animation.FuncAnimation(fig, animate, interval=10000) #update every 10sec
+    ani = animation.FuncAnimation(fig, animate, interval = plot_interval * 1000) 
     plt.show()
 
 ser = serial.Serial(port= serial_port)
- 
+
 # set variables
 global xs,y1,y2,y3,y4,y5,max_count,count,list_lock,start
 max_count = 360
@@ -85,22 +86,21 @@ y2 = []
 y3 = []
 y4 = []
 y5 = []
-start = 1
-list_lock = 0
+start = True
+list_lock = False
 
 
 def onclick(x):
     global start
-    start = 0
-      
+
 
 if __name__ == '__main__':
-   
+
     plot_thread = threading.Thread(target=thread_plot)
     plot_thread.start()
-    
+
     secondago = instant = datetime.datetime.now() - datetime.timedelta(seconds = 1)
-    while start == 1:                         #read serial data
+    while start:                                #read serial data
         Ard_data = ser.readline()
         Ard_data = Ard_data.decode("utf-8","ignore")
         counter1 = Ard_data.count(' ')
@@ -114,21 +114,21 @@ if __name__ == '__main__':
             linetype = "s"
         if counter3 == 4  :                     # header line, ignore
             b,c,d,e,f= Ard_data.split(",")
-            linetype = "h"    
+            linetype = "h"
         if counter1 == 4 and counter2 == 5:     # valid data line
             b,c,d,e,f= Ard_data.split(" ")
             linetype = "d"
                                                 # write to plot list element
-            while list_lock == 1:
+            while list_lock:
                 time.sleep(0.01)
-            list_lock = 1
+            list_lock = False
             xs.append(t)
             y1.append(float(b))
             y2.append(float(c))
             y3.append(float(d))
             y4.append(float(e))
             y5.append(float(f))
-            
+
             if len(xs) > plot_length:          # delete oldest values
                 del xs[0]
                 del y1[0]
@@ -136,15 +136,15 @@ if __name__ == '__main__':
                 del y3[0]
                 del y4[0]
                 del y5[0]
-            list_lock = 0
+            list_lock = False
             count +=1
-          
-        if log == 1:                           # save to log file  
+
+        if log:                               # save to log file
 
             instant = datetime.datetime.now().replace(microsecond=0)
             hourago = instant - datetime.timedelta(hours = 1)  # new file starts at 01:00
             logfile = log_path + hourago.strftime("%y-%m-%d") +'.log'
-            if (os.path.exists(logfile)): 
+            if (os.path.exists(logfile)):
                 pass
             else :                                          # logfile does not exist, jettison a new one
                 print ('new', logfile, 'is starting')
